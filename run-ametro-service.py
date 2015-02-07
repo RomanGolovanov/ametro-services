1b04e7c31c3f533a3f3d0df710161f33542ede6c
Roman Golovanov <roman.golovanov@gmail.com>
2015-02-07 14:23:21 +0300

    test

--- run-ametro-service.py
+++ run-ametro-service.py
@@ -10,5 +10,6 @@ if not os.path.isdir(working_folder):
 
 maps = pmetro.download_map_index('http://pmetro.chpeks.com/')
 pmetro.store_map_index(maps, os.path.join(working_folder, 'maps.json'))
+
 pmetro.download_map_index_files(maps, working_folder)
 
