f8f72b38c432e5284b998614875c7366fcba4074
Roman Golovanov <roman.golovanov@gmail.com>
2018-09-18 01:11:40 +0300

    make pMetro index catalog in www folder from manually extracted files

--- run-synchronization.py
+++ run-synchronization.py
@@ -3,15 +3,19 @@ import datetime
 from globalization.provider import GeoNamesProvider
 from publishing.downloader import MapDownloader
 from publishing.importer import MapImporter
+from publishing.indexer import MapIndexer
 from publishing.publisher import publish_maps
 
 from settings import MAPS_SOURCE_URL, CACHE_PATH, TEMP_PATH, IMPORT_PATH, APP_LOG, FORCE_IMPORT, FORCE_REFRESH, \
-    PUBLISHING_PATH, GEONAMES_DB
+    PUBLISHING_PATH, GEONAMES_DB, MANUAL_PATH, PMETRO_PATH
 
 geonames_provider = GeoNamesProvider(GEONAMES_DB)
 
 APP_LOG.message('')
 APP_LOG.message('Synchronization started at %s' % (datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')))
+
+indexer = MapIndexer(MANUAL_PATH, PMETRO_PATH, TEMP_PATH, APP_LOG)
+indexer.make_index()
 
 cache = MapDownloader(MAPS_SOURCE_URL, CACHE_PATH, TEMP_PATH, APP_LOG, geonames_provider)
 cache.refresh(force=FORCE_REFRESH)
